a065e56ad167b89a4d1331b243a73354d4061852
Stephen Lauck <stephen.lauck@gmail.com>
2015-05-20 12:28:46 -0400

    remove comments

--- recipes/aws.rb
+++ recipes/aws.rb
@@ -1,8 +1,8 @@
-# ~/chef-repo/cookbooks/web/recipes/servers.rb
+
 require 'chef/provisioning/aws_driver'
 with_driver 'aws'
 
-# specify what's needed to create a machine
+
 with_machine_options({
   :bootstrap_options => {
     :instance_type => 'm3.medium',
